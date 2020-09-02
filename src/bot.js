73b51ad5c42b027e6e801079e716c86e8664990b
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-09-02 19:46:51 +0200

    responses

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const time = require("moment");
+
 const { Client, Message } = require('discord.js');
 const client = new Client();
 const PREFIX = "$";
@@ -17,9 +19,27 @@ client.on('message', (message) => {
 
     
 
-    if(message.content === 'hello') {
-        message.reply('hello there!');
+    else if (message.content.toLowerCase().match("(^hello|^hi|^yo|^sup|^anyone on)")) {
+        var a = [
+            "Hi",
+            "Hola",
+            "Hello",
+            "Meep"
+        ];
+        message.reply(arraygr(a));
+    }
+    else if (message.content.toLowerCase() === 'online?') {
+        message.reply("Yup, hay " + message.author.username)
+    }
+    else if (message.content.toLowerCase().match("time|what time is it")) {
+        message.reply("It is " + time().format('MMM DD h:mm A'));
     }
 })
+
+
+
+function arraygr (list) {
+    return list[Math.floor((Math.random()*list.length))];
+} 
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
