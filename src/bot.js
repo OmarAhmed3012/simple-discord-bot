79da351a89436f54588fc57ded4d54726de2c77b
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-09-02 17:14:43 +0200

    responses

--- src/bot.js
+++ src/bot.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 
 const { Client, Message } = require('discord.js');
 const client = new Client();
+const PREFIX = "$";
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in`);
 });
 
 client.on('message', (message) => {
-    console.log(`${message.author.tag} : ${message.content}`)
+
+    //to control bot messages
+    if(message.author.bot) return;
+    
+    //console.log(`${message.author.tag} : ${message.content}`)
+
+    
 
     if(message.content === 'hello') {
         message.reply('hello there!');
